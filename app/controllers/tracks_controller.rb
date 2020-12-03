532c8a8e08ece93a1050c51d75530810e80f7efe
augustoibarrola <augusto.t.ibarrola@gmail.com>
2020-12-02 20:48:04 -0500

    tracks create action only creates uniquely favorited tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -11,6 +11,15 @@ class TracksController < ApplicationController
   end
 
   def create
+    @tracks = Track.all 
+    
+    @track = Track.find_or_create_by(track_name: params[:track_name], track_artist: params[:track_artist], track_album: params[:track_album] )
+
+    @track.save
+    render json: {track: @track, message: " tracks#create successfull !", all_tracks: @tracks}
+
+
+
   end
 
   def destroy
